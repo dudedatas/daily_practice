0e8d4606e5ce55dbfd16ad0664e59e5d52722a07
dudedatas <mikecerna99@gmail.com>
2026-02-26 09:13:04 -0600

    Initial commit: daily practice until I'm a datascientist.

---
+++ python_loops.py
@@ -0,0 +1,17 @@
+
+# 2026-02-26 
+# Loops
+
+numbers = [1, 2, 3, 4]
+
+total = 0
+product = 1
+
+for a_number in numbers:
+    product *= a_number
+    total += a_number
+
+print(f"Product: {product}")
+print(f"Sum: {total}")
+
+
