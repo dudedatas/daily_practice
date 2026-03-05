10670ad61a7c2fa2838cd4ea744211a7103927e6
dudedatas <mikecerna99@gmail.com>
2026-03-05 06:39:40 -0600

    Certification_Practice

---
+++ python_practice.py
@@ -0,0 +1,27 @@
+# 3-5-2026
+# Certification Practice
+
+def feet_to_meters(feet):
+    meters = feet * 0.3048
+    return print(f"{feet} feet is {meters} meters.")
+
+def length_of_list(some_list):
+    count = 0
+    for i in some_list:
+        count +=1
+    return count
+
+
+my_dict = {"a":1}
+
+my_dict.update(b=2, c=3, d=4)
+
+def string_counter(string):
+    length = len(string)
+    print(f"The length of the string is {length}")
+    return length
+
+
+
+string_counter("How many characters are in this sentence that is an overglorified string")
+
